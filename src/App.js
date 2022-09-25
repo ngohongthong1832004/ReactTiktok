9201e91f1242046f34f833b08ca345594c42b197
ngohongthong <baphongpine@gmail.com>
2022-09-25 14:29:57 +0700

    25/9/2022

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className = 'css' style={{color : 'red',fontSize : '50px'}}>Chào mừng đến với bình nguyên vô tận</div>
+    </div>
+  );
+}
+
+export default App;
